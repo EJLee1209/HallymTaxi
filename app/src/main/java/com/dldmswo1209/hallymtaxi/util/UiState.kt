298eedd1ed109cbf59d3f538089af29c2fdc9f40
EJLee1209 <dldmswo1209@gmail.com>
2023-02-15 01:46:55 +0900

    Migrate MVVM(회원가입 부분 완료)

---
+++ app/src/main/java/com/dldmswo1209/hallymtaxi/util/UiState.kt
@@ -0,0 +1,8 @@
+package com.dldmswo1209.hallymtaxi.util
+
+sealed class UiState<out T> {
+    // Loading, Success, Failure
+    object Loading: UiState<Nothing>()
+    data class Success<out T>(val data: T): UiState<T>()
+    data class Failure(val error: String?): UiState<Nothing>()
+}
